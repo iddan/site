e1329ab2d910814c4d6e8b011566ef0fe6d2f3dd
Iddan Aaronsohn <mail@aniddan.com>
2018-10-01 23:34:59 +0300

    Making things work

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: `iddan`
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-netlify-cms`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/projects`,
+        name: "projects"
+      }
+    },
+    `gatsby-transformer-remark`
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,44 @@
 import React from "react";
+import { graphql } from "gatsby";
 
 import Layout from "../layout";
 import Project from "../Project";
-import reactUniversalMarkdown from "../../images/react-universal-markdown.png";
-import reactKeen from "../../images/react-keen.png";
-import hackTheWeb from "../../images/hack-the-web.png";
 import "./index.css";
 
-export default () => (
+function* transformData(data) {
+  for (const edge of data.allMarkdownRemark.edges) {
+    yield edge.node.frontmatter;
+  }
+}
+
+export default ({ data }) => (
   <Layout>
     <main className="Home">
       <h2>Projects</h2>
       <section className="Projects">
-        <Project
-          title="Stylesheet"
-          description="Dynamic CSS for user interfaces"
-          image="https://camo.githubusercontent.com/15ab283599e3318fe60dd4944b109fea503e9d75/68747470733a2f2f63646e2e7261776769742e636f6d2f696464616e2f7374796c6573686565742f6d61737465722f6173736574732f7374796c6573686565742e737667"
-          imageSize={0.6}
-          link="https://iddan.github.io/stylesheet"
-        />
-        <Project
-          title="react-native-canvas"
-          description="A Canvas component for React Native"
-          image="https://camo.githubusercontent.com/7a4367d9df9f5e7d7ae8545c241749563d677507/68747470733a2f2f656d6f6a6970656469612d75732e73332e616d617a6f6e6177732e636f6d2f7468756d62732f3234302f6170706c652f39362f66697265776f726b735f31663338362e706e67"
-          imageSize={0.5}
-          link="https://github.com/iddan/react-native-canvas"
-        />
-        <Project
-          title="react-universal-markdown"
-          description="Markdown component for Web and Native powered by CommonMark"
-          image={reactUniversalMarkdown}
-          link="https://github.com/iddan/react-universal-markdown"
-        />
-        <Project
-          title="react-keen"
-          description="React bindings for Keen IO SDK"
-          image={reactKeen}
-          link="https://github.com/iddan/react-keen"
-          imageSize={1.1}
-        />
-        <Project
-          title="progress-circle"
-          description="Circular progress bar made with React, SVG and CSS Transitions."
-          image="https://camo.githubusercontent.com/d086a2d92f35eb1a5437fcf84db4a636174daf35/687474703a2f2f692e67697068792e636f6d2f6c337132546d73563946754e7a7961624b2e676966"
-          link="https://github.com/iddan/progress-circle"
-        />
-        <Project
-          title="Hack the Web"
-          description="A yearly event focusing on disrupting knowledge related to development for the World Wide Web."
-          image={hackTheWeb}
-          link="https://hacktheweb.org/"
-          imageSize={1}
-        />
+        {Array.from(transformData(data), (project, index) => (
+          <Project key={index} {...project} />
+        ))}
       </section>
     </main>
   </Layout>
 );
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            description
+            image
+            imageSize
+            link
+            startDate
+          }
+        }
+      }
+    }
+  }
+`;
