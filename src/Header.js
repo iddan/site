f57ecb234c6af4cd7c5315f4ae6241d32ce5aaf8
Iddan Aaronsohn <mail@aniddan.com>
2020-04-10 23:13:33 +0300

    Add dark mode toggle

--- src/Header.js
+++ src/Header.js
@@ -6,23 +6,49 @@ import twitter from "../icons/twitter.svg";
 import github from "../icons/github.svg";
 import instagram from "../icons/instagram.svg";
 import "./Header.css";
+import useDarkMode from "use-dark-mode";
 
-const SocialMedia = ({ link, icon }) => (
+const SocialMedia = ({ link, icon, alt }) => (
   <a className="SocialMedia" href={link}>
-    <img src={icon} />
+    <img alt={alt} src={icon} />
   </a>
 );
+
+const DarkModeToggle = () => {
+  const { value, toggle } = useDarkMode();
+  const emoji = value ? "🌙" : "☀️";
+  return (
+    <div className="DarkModeToggle" onClick={toggle}>
+      {emoji}
+    </div>
+  );
+};
 
 const Header = () => (
   <header>
     <h1>
       <Link to="/">iddan</Link>
     </h1>
+    <section className="settings">
+      <DarkModeToggle />
+    </section>
     <aside>
-      <SocialMedia link="https://facebook.com/theiddan" icon={facebook} />
-      <SocialMedia link="https://twitter.com/iddan" icon={twitter} />
-      <SocialMedia link="https://github.com/iddan" icon={github} />
-      <SocialMedia link="https://instagram.com/aniddan" icon={instagram} />
+      <SocialMedia
+        alt="Facebook"
+        link="https://facebook.com/theiddan"
+        icon={facebook}
+      />
+      <SocialMedia
+        alt="Twitter"
+        link="https://twitter.com/iddan"
+        icon={twitter}
+      />
+      <SocialMedia alt="GitHub" link="https://github.com/iddan" icon={github} />
+      <SocialMedia
+        alt="Instagram"
+        link="https://instagram.com/aniddan"
+        icon={instagram}
+      />
     </aside>
   </header>
 );
