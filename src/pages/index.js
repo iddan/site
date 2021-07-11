518f7f302a186400e545c0de44273cf8c8046e87
Iddan Aaronsohn <mail@aniddan.com>
2021-07-11 13:42:34 +0000

    Update home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,26 @@ import Layout from "../layout";
 import SEO from "../SEO";
 import "./index.css";
 
-export default () => {
+const Home = () => {
   return (
     <Layout>
       <SEO title="Home" />
       <div className="Home">
         <h3>hello,</h3>
+        <p>I'm Iddan Aaronsohn, an entrepreneur.</p>
+        <p>I specialise in digital products and software engineering.</p>
+        <p>Occasionally, I do graphic design and fine art.</p>
+        <p>Currently Based in Tel Aviv - Yafo.</p>
         <p>
-          I'm Iddan Aaronsohn a Software Engineer / Graphic Designer / Visual
-          Artist
+          I am working my way towards{" "}
+          <a href="https://en.wikipedia.org/wiki/Earning_to_give">
+            earn to give
+          </a>
+          .
         </p>
-        <p>Based in Tel Aviv - Yafo.</p>
       </div>
     </Layout>
   );
 };
+
+export default Home;
