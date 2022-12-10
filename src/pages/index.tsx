ba710dc9772dc9cf125e35a9df813a8e3627fe04
Iddan Aaronsohn <mail@aniddan.com>
2022-12-10 23:20:10 +0200

    Update the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,14 @@ const Home = () => {
     <Layout>
       <SEO title="Home" />
       <div className="Home">
-        <h3>hello,</h3>
+        <h3>Hello!</h3>
         <p>I'm Iddan Aaronsohn, an entrepreneur.</p>
         <p>I specialise in digital products and software engineering.</p>
         <p>Occasionally, I do graphic design and fine art.</p>
-        <p>Currently Based in Tel Aviv - Yafo.</p>
+        <p>
+          Based in <a href="https://en.wikipedia.org/wiki/Tel_Aviv">Tel Aviv</a>
+          .
+        </p>
         <p>
           I am working my way towards{" "}
           <a href="https://en.wikipedia.org/wiki/Earning_to_give">
